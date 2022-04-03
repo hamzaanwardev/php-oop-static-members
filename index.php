cba85f99c4e65efbb3f7c74a4526844818b04a51
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2022-04-03 17:01:07 +0500

    Details of Static Members

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP Static Members</title>
+</head>
+<body>
+    <h1>PHP OOP Static Members</h1>
+    <p>In PHP OOP whenever we make a class and also the properties and methods of that class then we have to make objects of them in order use them.</p>
+    <p>But with Static Members we can use properties and methods of class without making object.</p>
+    <p>You will be really thinking in PHP Object Oriented Programming how can we use Properties and classes without making object So, have a look Please how i am doing this?</p>
+
+    
+</body>
+</html>
