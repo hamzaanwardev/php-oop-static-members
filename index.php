6223c8921ebdda3a20113ab83b2a61dc5adb2141
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2022-04-03 17:20:18 +0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <p>In PHP OOP whenever we make a class and also the properties and methods of that class then we have to make objects of them in order use them.</p>
     <p>But with Static Members we can use properties and methods of class without making object.</p>
     <p>You will be really thinking in PHP Object Oriented Programming how can we use Properties and classes without making object So, have a look Please how i am doing this?</p>
-
+    <p>We will not use this-> while using static members becvause this is used while making objects. We will use self:: instead of this-></p>
     <?php
         class personal {
             public static $name = "Hamza Anwar";
@@ -26,5 +26,7 @@
         // $test = new personal();
         // $test->show();
     ?>
+
+    <h2>Using Static Members In Inherit class.</h2>
 </body>
 </html>
